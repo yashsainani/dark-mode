c644374802597ffba38d7049ad17e615a1a592eb
yashsainani <yashsainani1803@gmail.com>
2024-10-23 15:58:21 +0530

    updated the button

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ const toggler = document.getElementById('toggler');
 const button = document.getElementById('button');
 const p = document.getElementsByTagName('p')[0];
 
-let chk = true;
+// let toDark = true;
+// toggler.addEventListener('click', () => {
+//     if (toDark) {
+//         toggler.classList.add('toggled');
+//         body.classList.add('toggled-body');
+//         button.classList.add('toggled-button');
+//         p.classList.add('toggled-p');
+//         toDark = false;
+//     }
+//     else {
+//         toggler.classList.remove('toggled');
+//         body.classList.remove('toggled-body');
+//         button.classList.remove('toggled-button');
+//         p.classList.remove('toggled-p');
+//         toDark = true;
+//     }
+// });
 
 toggler.addEventListener('click', () => {
     toggler.classList.toggle('toggled');
     body.classList.toggle('toggled-body');
     button.classList.toggle('toggled-button');
     p.classList.toggle('toggled-p');
-})
+});
